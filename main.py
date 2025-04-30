39beb8d615da35700c048a126fd6ba34383e988d
Victor <victor21.peralta@gmail.com>
2025-04-30 18:48:12 -0500

    did some preprocessing and beggining of a model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Below are just how the data is layed out, will probably delete later.
 #id: Unique identifer for transaction
 #V1-V28: Anonomyized features, various attributes (time, location etc.)
-#Class: Binary Label indicating whether transaction is fraudulent or not
+#Amount: the transaction amount
+#Class: Binary Label indicating whether transaction is fraudulent or not. Binary 1 for and 0 for not
