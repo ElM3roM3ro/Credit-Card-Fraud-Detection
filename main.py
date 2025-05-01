4abe299f72b93da7cb25a874e1acdf8ad7f852e4
Victor <victor21.peralta@gmail.com>
2025-05-01 16:20:10 -0500

    created two models, still just have to write main so we can do the comparison and anything else we might want to do for this project

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 #V1-V28: Anonomyized features, various attributes (time, location etc.)
 #Amount: the transaction amount
 #Class: Binary Label indicating whether transaction is fraudulent or not. Binary 1 for and 0 for not
+
+
+#The notebook - fraud_detection.ipynb is where I created both of the models
+# There is also just some stuff in there where I was playing around
+#trying to see things about the dataset. Models folder holds the models
+
